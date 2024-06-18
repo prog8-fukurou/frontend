8a8f758c1c3bece7bb0d7731399824604725428a
haru <hinagoro5656@gmail.com>
2024-06-18 22:41:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ライブラリインストール
+
+```
+cd frontend
+npm i
+```
+
+# 実行
+
+```
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="container mx-auto my-20 text-2xl">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
