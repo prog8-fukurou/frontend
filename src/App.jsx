67eb0dbeb52bcb0bc618ba7fdc99993544ed0ffb
haru <hinagoro5656@gmail.com>
2024-06-19 12:42:50 +0900

    typescriptに変更&shadcn/uiの導入

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className="container mx-auto my-20 text-2xl">Hello world!</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div className="container my-20">
+        <h1 className="text-2xl mb-4">Hello World!</h1>
+        <Button>Click me!</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
