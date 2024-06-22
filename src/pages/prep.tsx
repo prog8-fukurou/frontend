4ce10fc48ab8b4d585607ec35b3179d4a6632313
sasanqu_a <sazanka0316@gmail.com>
2024-06-23 03:20:07 +0900

    feat: Update Prep page layout and styles

--- src/pages/prep.tsx
+++ src/pages/prep.tsx
@@ -1,14 +1,12 @@
 import { ChevronLeft } from 'lucide-react';
-import { Label } from '@radix-ui/react-label';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Link } from 'react-router-dom';
-import { CircleHelp } from 'lucide-react';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 
 const Prep = () => {
 	return (
-		<div className="h-screen p-6">
-			<div className="flex w-full m-12">
+		<div className="h-screen w-screen flex flex-col">
+			<div className="h-1/6 flex w-full p-12">
 				<div className="w-1/3">
 					<Button className="text-white bg-sky-700">
 						<ChevronLeft />
@@ -17,22 +15,42 @@ const Prep = () => {
 				</div>
 				<div className="w-2/3"></div>
 			</div>
-			<div className="w-full flex items-center justify-center">
-				<div className="w-2/3 text-center font-semibold text-2xl">参加者を募集しています...</div>
+			<div className="h-1/6 w-full flex items-center justify-center">
+				<div className="w-2/3 text-center font-semibold text-2xl">他のユーザーの参加を待っています... ( N / 4 )</div>
 				<div className="w-1/3 flex items-center">
 					<div className="max-w-96 flex gap-2">
-						<Input type="text" id="roomId" placeholder="部屋コード" />
+						<Input type="text" id="roomId" disabled />
 						<Button size={'sm'} className="text-base bg-sky-700">
 							Copy
 						</Button>
 					</div>
 				</div>
 			</div>
-			<div className="h-full container flex items-center justify-center">
-				<Button size={'lg'} className="text-base bg-sky-700">
-					ゲームスタート
-				</Button>
-				<p className="text-center text-gry-500">制限時間は１分です</p>
+			<div className="h-2/3 flex items-center justify-center">
+				<div className="w-96">
+					<div>参加中のユーザー</div>
+					<Table>
+						<TableBody>
+							<TableRow>
+								<TableCell className="font-medium">test user 1 ( You ) </TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell className="font-medium">test user 2</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
+				</div>
+			</div>
+			<div className="h-1/6 container flex items-center justify-center">
+				<div className="w-4/5"></div>
+				<div className="w-1/5 flex items-center justify-center">
+					<div>
+						<p className="text-center text-gry-500 mb-3">制限時間は１分です</p>
+						<Button size={'lg'} className="text-base bg-sky-700">
+							ゲームスタート
+						</Button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
