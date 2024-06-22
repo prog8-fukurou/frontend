d5fb1bc631c13a28d9ba41cb10d3101d19133a28
haru <hinagoro5656@gmail.com>
2024-06-23 00:16:59 +0900

    Merge branch 'develop' of https://github.com/prog8-fukurou/frontend into develop

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import Start from '@/pages/Start';
-import Playing from '@/pages/Playing';
+import GameRoom from "@/pages/GameRoom";
+import Start from "@/pages/Start";
+import Playing from "@/pages/Playing";
 import ComponentTest from '@/pages/ComponentTest';
-import { RouteObject } from 'react-router-dom';
+import { RouteObject } from "react-router-dom";
+import MasterWait from "@/pages/MasterWait";
 
 export const AppRoutes: RouteObject[] = [
-	{ path: '/', element: <Start /> },
-	{ path: '/playing', element: <Playing /> },
+  { path: "/", element: <Start /> },
+  { path: "/playing", element: <Playing /> },
+  { path: "/room", element: <GameRoom /> },
+  { path: "/master", element: <MasterWait /> },
 	{ path: '/debug', element: <ComponentTest /> },
 ];
