5a7e71479b402537cb2964b488e00aa89e4925e8
sasanqu_a <sazanka0316@gmail.com>
2024-06-23 02:38:14 +0900

    feat: Add Select page to AppRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import Start from '@/pages/Start';
 import Playing from '@/pages/Playing';
+import Select from '@/pages/Select';
 import { RouteObject } from 'react-router-dom';
 
 export const AppRoutes: RouteObject[] = [
 	{ path: '/', element: <Start /> },
 	{ path: '/playing', element: <Playing /> },
+	{ path: '/select', element: <Select /> },
 ];
