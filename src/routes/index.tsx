6959306d2f17275a202c207cd3b29ebcb3a237a1
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-06-22 17:34:25 +0900

    feat: Add Prep page to the app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import Start from '@/pages/Start';
 import Playing from '@/pages/Playing';
+import Prep from '@/pages/prep';
 import { RouteObject } from 'react-router-dom';
 
 export const AppRoutes: RouteObject[] = [
 	{ path: '/', element: <Start /> },
 	{ path: '/playing', element: <Playing /> },
+	{ path: '/prepare', element: <Prep /> },
 ];
