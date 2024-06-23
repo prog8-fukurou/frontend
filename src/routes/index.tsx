952f4c30748af471ded39fd9344ce5a947bf4d32
haru <hinagoro5656@gmail.com>
2024-06-23 10:43:36 +0900

    試しのGameRoomを削除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import GameRoom from "@/pages/GameRoom";
 import Start from "@/pages/Start";
 import Playing from "@/pages/Playing";
 import Prep from "@/pages/Prep";
@@ -8,7 +7,6 @@ import MasterWait from "@/pages/MasterWait";
 export const AppRoutes: RouteObject[] = [
   { path: "/", element: <Start /> },
   { path: "/playing", element: <Playing /> },
-  { path: "/room", element: <GameRoom /> },
   { path: "/master", element: <MasterWait /> },
   { path: "/prepare", element: <Prep /> },
 ];
