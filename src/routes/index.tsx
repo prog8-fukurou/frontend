10d0b34e9a6c6274b60da8805b13bc41c60e0338
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-06-23 11:16:29 +0900

    chore: Fix typo in import statement for Prep page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import Start from '@/pages/Start';
 import Playing from '@/pages/Playing';
 import Vote from '@/pages/Vote';
-import Prep from '@/pages/prep';
-import MasterWait from "@/pages/MasterWait";
+import Prep from '@/pages/Prep';
+import MasterWait from '@/pages/MasterWait';
 import { RouteObject } from 'react-router-dom';
 
 export const AppRoutes: RouteObject[] = [
 	{ path: '/', element: <Start /> },
 	{ path: '/playing', element: <Playing /> },
 	{ path: '/vote', element: <Vote /> },
-  { path: "/master", element: <MasterWait /> },
+	{ path: '/master', element: <MasterWait /> },
 	{ path: '/prepare', element: <Prep /> },
 ];
